88d1e1da07d7a86600d440222a2e47c73e09be63
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-28 19:56:19 +0300

    Шаблон MVVM по-простому. Lifecycle.

---
+++ app/src/main/java/com/pk4us/mvvm/GetData.kt
@@ -0,0 +1,18 @@
+package com.pk4us.mvvm
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class GetData:LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun getData(){
+        println("get data")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun sendData(){
+        println("send data")
+    }
+}
---
+++ app/src/main/java/com/pk4us/mvvm/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pk4us.mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pk4us.mvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private var getData = GetData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        lifecycle.addObserver(getData)
+    }
+
+}
